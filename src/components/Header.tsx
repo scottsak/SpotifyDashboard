4b6509a69835d86bd80e3eafb96fe9957f6670d4
Caleb Elliott <caleb123elliott@gmail.com>
2024-05-01 19:50:42 -0700

    functional bookmark bubbles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import useBookmarks from "../hooks/useBookmarks"
+import { Bookmark } from "../types/types";
 
 
 type HeaderProps = {};
@@ -7,13 +8,22 @@ const Header: React.FC<HeaderProps> = () => {
   const { bookmarks } = useBookmarks()
   return (
     <>
-      <h1 className="text-xl text-primary font-bold">Spotif12332y App</h1>
-      {
-        bookmarks?.map(bookmark => {
-          const { faviconUrl, url } = bookmark || {}
-          return <img src={faviconUrl} alt={url} />
-        })
-      }
+      <div className="flex w-full justify-center">
+        <div className="flex md:w-1/2 w-full justify-around">
+          {
+            bookmarks?.filter(({ url, faviconUrl }) => url && faviconUrl).slice(0, 5).map((bookmark: Bookmark) => {
+              const { faviconUrl, url } = bookmark || {}
+              return (
+                <a href={url}>
+                  <div className="bg-dark p-3 rounded-full">
+                    <img src={faviconUrl} alt={url} className="w-6 contain" />
+                  </div>
+                </a>
+              )
+            })
+          }
+        </div>
+      </div>
     </>
   )
 }
