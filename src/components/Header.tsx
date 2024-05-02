d3e17897a993c52794c31cb0ca2e14547d34df06
Caleb Elliott <caleb123elliott@gmail.com>
2024-05-01 20:49:12 -0700

    header updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,14 +8,14 @@ const Header: React.FC<HeaderProps> = () => {
   const { bookmarks } = useBookmarks()
   return (
     <>
-      <div className="flex w-full justify-center">
-        <div className="flex md:w-1/2 w-full justify-around">
+      <div className="flex w-full justify-center bg-darker p-4">
+        <div className="flex md:w-3/4 lg:w-1/2 w-full justify-around">
           {
             bookmarks?.filter(({ url, faviconUrl }) => url && faviconUrl).slice(0, 5).map((bookmark: Bookmark) => {
               const { faviconUrl, url } = bookmark || {}
               return (
                 <a href={url}>
-                  <div className="bg-dark p-3 rounded-full">
+                  <div className="bg-dark p-3 rounded-full mx-2">
                     <img src={faviconUrl} alt={url} className="w-6 contain" />
                   </div>
                 </a>
--- src/hooks/useBookmarks.ts
+++ src/hooks/useBookmarks.ts
@@ -2,8 +2,12 @@ import { useEffect, useState } from "react";
 import { Bookmark } from "../types/types";
 
 type SetBookmarks = (bookmarks: Bookmark[] | null) => void;
+type SetError = (error: Boolean) => void;
+type SetLoading = (loading: Boolean) => void;
 type UseBookmarksReturnValue = {
-  bookmarks: Array<Bookmark> | null
+  bookmarks: Array<Bookmark> | null,
+  error: Boolean,
+  loading: Boolean
 }
 
 
@@ -17,28 +21,36 @@ const getFaviconUrl = (bookmarkUrl: string | undefined): string => {
   return url.toString();
 };
 
-const getBookmarks = async (setBookmarks: SetBookmarks) => {
-  const bookmarks = await chrome.bookmarks.getTree();
-  const [{ children }] = bookmarks || [{} as Bookmark];
+const getBookmarks = async (setBookmarks: SetBookmarks, setError: SetError, setLoading: SetLoading) => {
+  try {
+    setLoading(true)
+    const bookmarks = await chrome.bookmarks.getTree();
+    const [{ children }] = bookmarks || [{} as Bookmark];
 
-  const barBookmarks = ((children || []).find(({ title }) => title === 'Bookmarks Bar') || {}).children || [];
-  const bookmarksWithFavicon: Array<Bookmark> =
-    barBookmarks.map((bookmark: Bookmark) => ({
-      ...bookmark,
-      faviconUrl: getFaviconUrl(bookmark.url),
-    }))
-
-  setBookmarks(bookmarksWithFavicon || null);
+    const barBookmarks = ((children || []).find(({ title }) => title === 'Bookmarks Bar') || {}).children || [];
+    const bookmarksWithFavicon: Array<Bookmark> =
+      barBookmarks.map((bookmark: Bookmark) => ({
+        ...bookmark,
+        faviconUrl: getFaviconUrl(bookmark.url),
+      }))
+    setBookmarks(bookmarksWithFavicon || null);
+  } catch (err) {
+    setError(true)
+  } finally {
+    setLoading(false)
+  }
 };
 
 const useBookmarks = (): UseBookmarksReturnValue => {
   const [bookmarks, setBookmarks] = useState<Bookmark[] | null>(null);
+  const [error, setError] = useState<Boolean>(false);
+  const [loading, setLoading] = useState<Boolean>(false);
 
   useEffect(() => {
-    getBookmarks(setBookmarks);
+    getBookmarks(setBookmarks, setError, setLoading);
   }, []);
 
-  return { bookmarks };
+  return { bookmarks, error, loading };
 };
 
 export default useBookmarks;
